b7cd6618f512325a174365880a2b7b245850c6a4
qamiralii <a.bitpins@gmail.com>
2025-05-04 14:21:57 +0330

    making routers_2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,15 @@
 import { Routes } from '@angular/router';
+import { PublicNavComponent } from './+navigations/public-nav/ui/public-nav.component';
+import { PrivateNavComponent } from './+navigations/private-nav/ui/private-nav.component';
 
 export const routes: Routes = [
-    
+    {
+        path: "public", component: PublicNavComponent, children: [
+            { path: 'home' }
+        ]
+
+    },
+    {
+        path: 'private', component:PrivateNavComponent, children:[]
+    }
 ];
