612bc114c9856f9cc30818f204297bee1d337b94
qamiralii <a.bitpins@gmail.com>
2025-05-05 00:42:42 +0330

    start head & body

--- src/app/+navigations/public-nav/ui/public-nav.component.ts
+++ src/app/+navigations/public-nav/ui/public-nav.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { FlowbiteService } from '../../../+services/flowbite.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-public-nav',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './public-nav.component.html',
   styleUrl: './public-nav.component.css'
 })
-export class PublicNavComponent {
+export class PublicNavComponent implements OnInit {
+  constructor(private flowbiteService: FlowbiteService) { }
 
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }
--- src/app/+pages/home/ui/home.component.ts
+++ src/app/+pages/home/ui/home.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { FlowbiteService } from '../../../+services/flowbite.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet,],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private flowbiteService: FlowbiteService) { }
 
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }
