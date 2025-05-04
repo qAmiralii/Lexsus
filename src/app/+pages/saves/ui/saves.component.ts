b1b0ad967e0702a24f3b98b6266d8f2f73f4070a
qamiralii <a.bitpins@gmail.com>
2025-05-04 23:29:59 +0330

    making routers_3

---
+++ src/app/+pages/saves/ui/saves.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saves',
+  imports: [],
+  templateUrl: './saves.component.html',
+  styleUrl: './saves.component.css'
+})
+export class SavesComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,21 @@
 import { Routes } from '@angular/router';
 import { PublicNavComponent } from './+navigations/public-nav/ui/public-nav.component';
 import { PrivateNavComponent } from './+navigations/private-nav/ui/private-nav.component';
+import { HomeComponent } from './+pages/home/ui/home.component';
+import { SavesComponent } from './+pages/saves/ui/saves.component';
 
 export const routes: Routes = [
     {
         path: "public", component: PublicNavComponent, children: [
-            { path: 'home' }
+            { path: '', redirectTo: 'home', pathMatch: 'prefix' },
+            { path: 'home', component: HomeComponent },
+            { path: 'saves' , component: SavesComponent}
+
         ]
 
     },
     {
-        path: 'private', component:PrivateNavComponent, children:[]
-    }
+        path: 'private', component: PrivateNavComponent, children: []
+    },
+    { path: '', redirectTo: 'public', pathMatch: 'full' }
 ];
